360b858533b2c76906821fcaa675876f7830175e
Kipsazz <kbickus@gmail.com>
2020-07-17 13:20:26 +0300

    paslaugos ir patarimai sub page classSelected fixed

--- js/cookies.js
+++ js/cookies.js
@@ -1,13 +1,14 @@
+/* TODO add exp. data name  https://www.w3schools.com/js/tryit.asp?filename=tryjs_cookie_username */
 const cookieContainer = document.querySelector(".cookie-container");
 const cookieButton = document.querySelector(".cookie-btn");
 
 cookieButton.addEventListener("click", () => {
-  cookieContainer.classList.remove("active");
-  localStorage.setItem("AutremisCookieBannerDisplayed", "true");
+	cookieContainer.classList.remove("active");
+	document.cookie = ("AutremisCookieBannerDisplayed", "true");
 });
 
 setTimeout(() => {
-  if (!localStorage.getItem("AutremisCookieBannerDisplayed")) {
-    cookieContainer.classList.add("active");
-  }
+	if (document.cookie < ("AutremisCookieBannerDisplayed")) {
+		cookieContainer.classList.add("active");
+		}
 }, 2000);
